a2fd5050065bd7b548cf3e3d25a015a69ff7db11
abarovi <abarovi2@uccs.edu>
2023-04-24 21:25:40 +0000

    after Login with 54 coverage

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,10 @@
 class Project < ApplicationRecord
     validates_presence_of :name, :Class_Type, :Class_Number, :Class_Start, :Class_Finish, :Contact_Info
+    def self.search(search)
+        if search
+          where('name LIKE ?', "%#{search}%")
+        else
+          all
+        end
+    end
 end
--- spec/features/projects_feature_spec.rb
+++ spec/features/projects_feature_spec.rb
@@ -1,14 +1,48 @@
 require "rails_helper"
 
 RSpec.feature "Projects", type: :feature do
+  context "Login" do
+    scenario "should sign up" do
+      visit root_path
+      click_link 'Sign up'
+      within("form") do
+        fill_in "Email", with: "testing@test.com"
+        fill_in "Password", with: "123456"
+        fill_in "Password confirmation", with: "123456"
+        click_button "Sign up"
+      end
+      expect(page).to have_content("Welcome! You have signed up successfully.")
+    end
+
+
+    scenario "should log in" do
+      user = FactoryBot.create(:user)
+      login_as(user)
+      visit root_path
+      expect(page).to have_content("Logged in")
+    end
+    
+  end
+
     context "Update project" do
       let(:project) { Project.create(name: "Andrew", Class_Type: "CS", Class_Number: 1234, Class_Start: Date.today, Class_Finish: Date.today, Contact_Info: "Contact Me") }
       before(:each) do
+        user = FactoryBot.create(:user)
+        login_as(user)
         visit edit_project_path(project)
       end
       scenario "should be successful" do
         within("form") do
           fill_in "Name", with: "Barovic"
+        end
+        click_button "Save to Database"
+        expect(page).to have_content("Project was successfully updated")
+      end
+      scenario "should be successful" do
+        within("form") do
+          fill_in "Name", with: "Barovic"
+          fill_in "Class Type", with: "BAR"
+          fill_in "Class Number", with: 12345
         end
         click_button "Save to Database"
         expect(page).to have_content("Project was successfully updated")
@@ -27,9 +61,26 @@ RSpec.feature "Projects", type: :feature do
         click_button "Save to Database"
         expect(page).to have_content("Class type can't be blank")
       end
+      scenario "should fail" do
+        within("form") do
+          fill_in "Class Number", with: ""
+        end
+        click_button "Save to Database"
+        expect(page).to have_content("Class number can't be blank")
+      end
+      scenario "should fail" do
+        within("form") do
+          fill_in "Contact Info", with: ""
+        end
+        click_button "Save to Database"
+        expect(page).to have_content("Contact info can't be blank")
+      end
     end
     
     scenario "User creates a new project with valid attributes" do
+      user = FactoryBot.create(:user)
+      login_as(user)
+
         visit new_project_path
     
         fill_in "Name", with: "Test Student"
@@ -46,6 +97,9 @@ RSpec.feature "Projects", type: :feature do
         expect(page).to have_content("Contact Me!")
       end
       scenario "User attempts to create a new project with invalid attributes" do
+        user = FactoryBot.create(:user)
+        login_as(user)
+
         visit new_project_path
     
         click_button "Save to Database"
